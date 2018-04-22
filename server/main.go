7f3cd4c5fb60ea4bbd31b58422041aadc95cf9da
Sun1ive <sunliveua@gmail.com>
2018-04-22 13:37:07 +0300

    gettin better

--- server/main.go
+++ server/main.go
@@ -1,3 +1,26 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"log"
+	"github.com/gorilla/mux"
+)
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/todos", GetTodos).Methods("GET")
+	// myRouter.HandleFunc("/user/{name}/{email}", NewUser).Methods("POST")
+	// myRouter.HandleFunc("/user/{name}", DeleteUser).Methods("DELETE")
+	// myRouter.HandleFunc("/user/{name}/{email}", UpdateUser).Methods("PUT")
+
+	fmt.Println("Server is running at port 3000")
+	log.Fatal(http.ListenAndServe(":3000", myRouter))
+}
+
+
+func main() {
+	InitialMigration()
+	handleRequests()
+}
