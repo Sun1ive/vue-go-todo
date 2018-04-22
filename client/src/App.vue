0e64ca6f5319cb6d458d448c3df3f9ba78c488fd
Sun1ive <sunliveua@gmail.com>
2018-04-22 13:14:01 +0300

    gettin better

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,51 @@
 <template>
-
+  <v-app>
+    <v-container fluid>
+      <v-layout justify-center>
+        <v-flex xs10 sm6 lg4>
+          <v-card>
+            <v-card-title>
+              <h2>Vue.js + GO TodoList</h2>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+      </v-layout>
+      <v-layout justify-center>
+        <v-flex xs10 sm6 lg4 class="text-xs-center">
+          <v-text-field v-model.lazy.trim="todo" />
+          <v-btn
+            dark
+            color="primary"
+            @click="addTodo"
+          >Add Todo</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
 </template>
 
-<style lang="stylus">
+<script lang="ts">
+import Vue from 'vue';
+import api from './api/instance';
 
+export default Vue.extend({
+  data: () => ({
+    todo: '' as string,
+  }),
+  methods: {
+    async addTodo() {
+      api().post('/create', {});
+    },
+  },
+});
+</script>
+
+
+<style lang="stylus">
+#app
+  font-family Roboto
+
+.card__title
+  text-align center
+  justify-content center
 </style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
-import SuiVue from 'semantic-ui-vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.css';
 
 Vue.config.productionTip = false;
-
-Vue.use(SuiVue);
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#ff995e', // #E53935
+  },
+});
 
 new Vue({
   render: h => h(App),
