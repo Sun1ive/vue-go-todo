d6d3ae5a106b3b96fd207586826849740d423461
Sun1ive <sunliveua@gmail.com>
2018-04-23 13:12:13 +0300

    gettin better

--- server/main.go
+++ server/main.go
@@ -1,20 +1,43 @@
 package main
 
 import (
-	"log"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+// Todo Structure
 type Todo struct {
+	gorm.Model
 	Title    string `json:"title"`
 	Category string `json:"category"`
 	IsDone   string `json:"isdone"`
 }
 
+/* Globals variables */
+var db *gorm.DB
+var err error
 var todo Todo
 
+// connect to db
+func connect() {
+	db, err = gorm.Open("sqlite3", "test.sqlite")
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+}
+
+// InitialMigration for DB
+func InitialMigration() {
+	connect()
+	db.AutoMigrate(&Todo{})
+}
+
+// SetHeaders handler
 func SetHeaders(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
 }
