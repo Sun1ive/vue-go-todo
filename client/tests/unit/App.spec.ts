1d2c95c3ec7a3d2bb48cf3393b2c49d189cde74e
Sun1ive <sunliveua@gmail.com>
2018-04-23 11:16:55 +0300

    gettin better

--- client/tests/unit/App.spec.ts
+++ client/tests/unit/App.spec.ts
@@ -1,7 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import { shallow, mount, createLocalVue } from '@vue/test-utils';
+import sinon from 'sinon';
 import Vuetify from 'vuetify';
+import Vue from 'vue';
 import App from '../../src/App.vue';
 
 const localVue = createLocalVue();
@@ -15,11 +17,43 @@ describe('App.vue', () => {
     });
     expect(wrapper).not.toBe(null);
   });
-
-  it('Should render without crash', () => {
+  it('Should contain correct wrapper', () => {
     const wrapper = shallow(App, {
       localVue,
     });
     expect(wrapper).toBeInstanceOf(Object);
+  });
+  it('Should be vue instance', () => {
+    const wrapper = shallow(App, {
+      localVue,
+    });
+    expect(wrapper.isVueInstance()).toBe(true);
+  });
+  it('Should contain correct classes', () => {
+    const wrapper = shallow(App, {
+      localVue,
+    });
+    expect(wrapper.classes()).toContain('application');
+    expect(wrapper.classes()).toContain('theme--light');
+  });
+  it('Should set correct data', () => {
+    const todos = ['hello', 'world'];
+    const wrapper = mount(App, {
+      localVue,
+    });
+    wrapper.setData({ todos });
+    // @ts-ignore
+    expect(wrapper.vm.todos).toBe(todos);
+  });
+  it('Should set correct methods', () => {
+    const clickHandler = sinon.stub();
+    const wrapper = mount(App, {
+      localVue,
+      propsData: { clickHandler },
+    });
+    const divArray = wrapper.findAll('div');
+    expect(clickHandler.called).toBe(false);
+    divArray.trigger('click');
+    expect(clickHandler.called).toBe(true);
   });
 });
