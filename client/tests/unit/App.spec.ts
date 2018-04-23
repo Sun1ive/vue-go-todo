3bd230925247f81d61120d39b84e66b6449bf880
Sun1ive <sunliveua@gmail.com>
2018-04-23 10:37:11 +0300

    gettin better

---
+++ client/tests/unit/App.spec.ts
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import { shallow, mount, createLocalVue } from '@vue/test-utils';
+import Vuetify from 'vuetify';
+import App from '../../src/App.vue';
+
+const localVue = createLocalVue();
+
+localVue.use(Vuetify);
+
+describe('App.vue', () => {
+  it('Should render without crash', () => {
+    const wrapper = shallow(App, {
+      localVue,
+    });
+    expect(wrapper).not.toBe(null);
+  });
+});
