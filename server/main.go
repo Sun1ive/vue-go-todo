fe5ad2f2029073a839b4d2cbda8fa8be46e21d82
Sun1ive <sunliveua@gmail.com>
2018-04-23 13:00:07 +0300

    gettin better

--- server/main.go
+++ server/main.go
@@ -3,89 +3,45 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-var db *gorm.DB
-var err error
-
-// Todo Model struct
 type Todo struct {
-	gorm.Model
-	Title    string `json:"title"`
+	Title string `json:"title"`
 	Category string `json:"category"`
-	IsDone   string `json:"isDone"`
+	IsDone string `json:"isdone"`
 }
 
-// TODOS LIST
-var todos []Todo
-
-func setHeaders(res http.ResponseWriter) {
-	res.Header().Set("Content-Type", "application/json")
+func SetHeaders(w http.ResponseWriter)  {
+	w.Header().Set("Content-Type", "application/json")
 }
 
-func connect() {
-	db, err = gorm.Open("sqlite3", "test.sqlite")
+func parseBody(w http.ResponseWriter, r *http.Request)  {
+	decoder := json.NewDecoder(r.Body)
+
+	var todo Todo
+	err := decoder.Decode(&todo)
+
 	if err != nil {
-		panic("Failed to connect to database")
+		panic(err)
 	}
+
+	fmt.Println(todo.Title)
+	fmt.Println(todo.Category)
+	fmt.Println(todo.IsDone)
+
+	output, err := json.Marshal(todo)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	SetHeaders(w)
+
+	w.Write(output)
 }
 
-// InitialMigration db
-func InitialMigration() {
-	connect()
-	defer db.Close()
-
-	db.AutoMigrate(&Todo{})
-}
-
-// GetTodos Endpoint
-func GetTodos(w http.ResponseWriter, r *http.Request) {
-	connect()
-	defer db.Close()
-
-	db.Find(&todos)
-
-	setHeaders(w)
-	json.NewEncoder(w).Encode(todos)
-}
-
-// CreateTodo handler
-func CreateTodo(w http.ResponseWriter, r *http.Request) {
-	// connect()
-	// defer db.Close()
-
-	// vars := mux.Vars(r)
-	// title := vars["title"]
-	// category := vars["category"]
-	// isDone := vars["isDone"]
-
-	// db.Create(&Todo{Title: title, Category: category, IsDone: isDone})
-	// setHeaders(w)
-	vars := mux.Vars(r)
-	title := vars["title"]
-	fmt.Println(title)
-	// fmt.Fprintf(w, "TODO CREATED")
-}
-
-func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-
-	myRouter.HandleFunc("/todos", GetTodos).Methods("GET")
-	myRouter.HandleFunc("/create/{title}", CreateTodo).Methods("POST")
-	// myRouter.HandleFunc("/user/{name}", DeleteUser).Methods("DELETE")
-	// myRouter.HandleFunc("/user/{name}/{email}", UpdateUser).Methods("PUT")
-
-	fmt.Println("Server is running at port 3000")
-	log.Fatal(http.ListenAndServe(":3000", myRouter))
-}
-
-func main() {
-	InitialMigration()
-	handleRequests()
+func main()  {
+	http.HandleFunc("/", parseBody)
+	http.ListenAndServe(":8081", nil)
 }
